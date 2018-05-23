4693dc9590438cb53b8177cc0020ca2f1a1eeef2
Kathy Luu <kathyluu820@gmail.com>
2018-05-23 11:59:23 -0700

    fancy text

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -1,13 +1,29 @@
+/*eslint-disable*/
 import React from 'react';
 import styled from 'styled-components';
+import ReactRevealText from 'react-reveal-text';
 
-const Container = styled.div``;
+const Name = styled.h1`
+  font-size: 48px;
+  text-transform: uppercase;
+`;
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
+
+  componentDidMount() {
+    this.setState({ show: true });
+  }
+
   render() {
     return (
       <div id="home" className="body">
-        <Container>Home</Container>
+        <Name>
+          <ReactRevealText show={this.state.show}>Kathy Luu</ReactRevealText>
+        </Name>
       </div>
     );
   }
